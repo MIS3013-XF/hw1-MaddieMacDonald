c11d7ac9a1c1305ef5040ecf36478fabf0eb9576
MaddieMacDonald <Madison.R.MacDonald-1@ou.edu>
2025-09-11 20:15:16 -0500

    Homework 1.

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,6 +1,6 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
+// Your Name: Maddie MacDonald
 // Did you seek help ? If yes, specify the helper or web link here: 
 
 using System;
@@ -15,6 +15,30 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What item are you buying?");
+            string item = Console.ReadLine();
+
+            Console.WriteLine("What is the quantity of your item?");
+            string quantity = Console.ReadLine();
+            double quantitydouble;
+            quantitydouble = Convert.ToDouble(quantity);
+
+            Console.WriteLine("What is the price of the item?");
+            string price = Console.ReadLine();
+            double pricedouble;
+            pricedouble = Convert.ToDouble(price);
+
+            const double salestax = 0.085;
+            double totalsalestax = (pricedouble * quantitydouble)*salestax;
+            double salestotal = ((pricedouble*quantitydouble) + totalsalestax);
+            double subtotal = (pricedouble*quantitydouble);
+
+            Console.WriteLine("Final subtotal: $" + subtotal.ToString("N2"));
+            Console.WriteLine("Final tax: $" + totalsalestax.ToString("N2"));
+            Console.WriteLine("Final sales price: $" + salestotal.ToString("N2"));
+
+            Console.ReadKey();
+
         }
     }
 }
